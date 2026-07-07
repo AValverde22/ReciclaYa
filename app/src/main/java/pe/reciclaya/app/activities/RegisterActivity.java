24bf11ca6a9a8304cff5099e42c4d46cc1a0e5f1
AValverde22 <valnor.alejandro@gmail.com>
2026-07-07 07:37:37 -0500

    Vistas creadas

--- app/src/main/java/pe/reciclaya/app/activities/RegisterActivity.java
+++ app/src/main/java/pe/reciclaya/app/activities/RegisterActivity.java
@@ -1,4 +1,4 @@
-package pe.reciclaya.app;
+package pe.reciclaya.app.activities;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import pe.reciclaya.app.R;
+
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
